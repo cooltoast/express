2942900cd7821adf0130d83ec1b02084ca584e2e
cooltoast <mpatel678@gmail.com>
2014-04-04 16:53:26 -0400

    made balls move

--- static/js/chain_reaction.js
+++ static/js/chain_reaction.js
@@ -19,15 +19,15 @@ $(document).ready(function() {
     radius: 10,
     x: width - 10, //keeps track of x position
     y: height - 10, //keeps track of y position
-    vx: 5, //keeps track of x velocity
-    vy: 5 //keeps track of y velocity
+    vx: -5, //keeps track of x velocity
+    vy: -5 //keeps track of y velocity
   };
 
   var b2 = {
     radius: 50,
     x: width - 50, //keeps track of x position
     y: 50, //keeps track of y position
-    vx: 5, //keeps track of x velocity
+    vx: -5, //keeps track of x velocity
     vy: 5 //keeps track of y velocity
   };
 
@@ -48,41 +48,29 @@ $(document).ready(function() {
 
     context.fillStyle = 'black';
 
-
     for (var i = 0; i < balls.length; i++)
     {
+      balls[i].x = balls[i].x + balls[i].vx;
+      balls[i].y = balls[i].y + balls[i].vy;
+
+      if((balls[i].x == width - balls[i].radius) || (balls[i].x == balls[i].radius))
+      {
+        balls[i].vx = -balls[i].vx;
+      }
+
+      if((balls[i].y == height - balls[i].radius) || (balls[i].y == balls[i].radius))
+      {
+        balls[i].vy = -balls[i].vy;
+      }
+    
       context.beginPath();
       context.arc(balls[i].x, balls[i].y, balls[i].radius, 0, 2*Math.PI);
       context.closePath();
       context.stroke();
       context.fill();
+    }
+
     
-      balls[i].x = balls[i].x + balls[i].vx;
-      balls[i].y = balls[i].y + balls[i].vy;
-
-    }
-
-
-    if((ball1.x == width - ball1.radius) || (ball1.x == ball1.radius))
-    {
-      ball1.vx = -ball1.vx;
-    }
-
-    if((ball1.y == height - ball1.radius) || (ball1.y == ball1.radius))
-    {
-      ball1.vy = -ball1.vy;
-    }
-
-    if((ball2.x == width - ball2.radius) || (ball2.x == ball2.radius))
-    {
-      ball2.vx = -ball2.vx;
-    }
-
-
-    if((ball2.y == height - ball2.radius) || (ball2.y == ball2.radius))
-    {
-      ball2.vy = -ball2.vy;
-    }
 
     setTimeout(updateGame, 10);
   };
