fb6f799c2d0f616aede21226eb28e72c502dc404
cooltoast <mpatel678@gmail.com>
2014-04-20 01:25:11 -0400

    added menu and counter

--- static/js/chain_reaction.js
+++ static/js/chain_reaction.js
@@ -5,6 +5,10 @@ $(document).ready(function() {
   var width = canvas.width;
   var height = canvas.height;
 
+  var gameState = "menu";
+
+  var menuText = "CLICK TO PLAY";
+
   var reacting = false;
 
   var numReacted = 0;
@@ -25,8 +29,8 @@ $(document).ready(function() {
 
     var b = {
       radius: 10,
-      x: width * Math.random(), //keeps track of x position
-      y: height * Math.random(), //keeps track of y position
+      x: (width - 20) * Math.random() + 10, //keeps track of x position
+      y: (height - 20) * Math.random() + 10, //keeps track of y position
       vx: modifier * 10 * Math.random(), //keeps track of x velocity
       vy: modifier * 10 * Math.random(), //keeps track of y velocity
       color: colors[Math.floor((Math.random()*colors.length))]
@@ -45,125 +49,171 @@ $(document).ready(function() {
 
     clearScreen();
 
-    for (var i = 0; i < balls.length; i++)
+    if (gameState == "menu")
     {
-      if((balls[i].x >= width - balls[i].radius) || (balls[i].x <= balls[i].radius))
-      {
-        balls[i].vx = -balls[i].vx;
-      }
+      context.fillStyle = "black";
 
-      if((balls[i].y >= height - balls[i].radius) || (balls[i].y <= balls[i].radius))
-      {
-        balls[i].vy = -balls[i].vy;
-      }
+      context.font = "50px Arial";
 
-      balls[i].x = balls[i].x + balls[i].vx;
-      balls[i].y = balls[i].y + balls[i].vy;
+      context.fillText(menuText, width/2 - 175, height/2 - 50);
 
-      context.fillStyle = balls[i].color;
-    
-      
-
-      context.beginPath();
-      context.arc(balls[i].x, balls[i].y, balls[i].radius, 0, 2*Math.PI);
-      context.closePath();
-      context.stroke();
-      context.fill();
-    
+      gameState == "playing";
     }
 
-    for (var i = 0; i < reactions.length; i++)
+    else if (gameState == "playing")
     {
-      reactions[i].timer = reactions[i].timer + 1;
-      
-      if((reactions[i].x >= width - reactions[i].radius) || (reactions[i].x <= reactions[i].radius))
+      for (var i = 0; i < balls.length; i++)
       {
-        reactions[i].vx = -reactions[i].vx;
-      }
-
-      if((reactions[i].y >= height - reactions[i].radius) || (reactions[i].y <= reactions[i].radius))
-      {
-        reactions[i].vy = -reactions[i].vy;
-      }
-
-      reactions[i].x = reactions[i].x + reactions[i].vx;
-      reactions[i].y = reactions[i].y + reactions[i].vy;
-
-      context.fillStyle = reactions[i].color;
-
-      context.beginPath();
-      context.arc(reactions[i].x, reactions[i].y, reactions[i].radius, 0, 2*Math.PI);
-      context.closePath();
-      context.stroke();
-      context.fill();
-
-      if (reactions[i].timer > 200)
-      {
-        reactions[i].radius = reactions[i].radius - 1;
-      }
-
-      else if (reactions[i].radius < 30)
-      {
-        reactions[i].radius = reactions[i].radius + 1;
-      }
-
-      if (reactions[i].radius == 0)
-      {
-        reactions.splice(i, 1);
-      }
-    }
-    
-    
-    for (var i = 0; i < balls.length; i++)
-    {
-      for (var j = 0; j < reactions.length; j++) 
-      {
-        var xdiff = (reactions[j].x - balls[i].x);
-        var ydiff = (reactions[j].y - balls[i].y);
-        var dist = Math.sqrt(xdiff * xdiff + ydiff * ydiff);
-        
-        //if collision, make new reaction object and add to array
-        if (dist <= (reactions[j].radius + balls[i].radius))
+        if((balls[i].x >= width - balls[i].radius) || (balls[i].x <= balls[i].radius))
         {
-          var new_reaction = {
-            radius: 10,
-            x: balls[i].x, //keeps track of x position
-            y: balls[i].y, //keeps track of y position
-            vx: 0, //keeps track of x velocity
-            vy: 0, //keeps track of y velocity
-            color: balls[i].color,
-            timer: 0
-          };
-
-          reactions.push(new_reaction);
-
-          numReacted = numReacted + 1;
-         
-          balls.splice(i, 1);
-
-          if (i >= 1)
-          {
-            i--;
-          }
-          
+          balls[i].vx = -balls[i].vx;
         }
 
+        if((balls[i].y >= height - balls[i].radius) || (balls[i].y <= balls[i].radius))
+        {
+          balls[i].vy = -balls[i].vy;
+        }
+
+        balls[i].x = balls[i].x + balls[i].vx;
+        balls[i].y = balls[i].y + balls[i].vy;
+
+        context.fillStyle = balls[i].color;
+      
+        
+
+        context.beginPath();
+        context.arc(balls[i].x, balls[i].y, balls[i].radius, 0, 2*Math.PI);
+        context.closePath();
+        context.stroke();
+        context.fill();
+      
+      }
+
+      for (var i = 0; i < reactions.length; i++)
+      {
+        reactions[i].timer = reactions[i].timer + 1;
+        
+        if((reactions[i].x >= width - reactions[i].radius) || (reactions[i].x <= reactions[i].radius))
+        {
+          reactions[i].vx = -reactions[i].vx;
+        }
+
+        if((reactions[i].y >= height - reactions[i].radius) || (reactions[i].y <= reactions[i].radius))
+        {
+          reactions[i].vy = -reactions[i].vy;
+        }
+
+        reactions[i].x = reactions[i].x + reactions[i].vx;
+        reactions[i].y = reactions[i].y + reactions[i].vy;
+
+        context.fillStyle = reactions[i].color;
+
+        context.beginPath();
+        context.arc(reactions[i].x, reactions[i].y, reactions[i].radius, 0, 2*Math.PI);
+        context.closePath();
+        context.stroke();
+        context.fill();
+
+        if (reactions[i].timer > 200)
+        {
+          reactions[i].radius = reactions[i].radius - 1;
+        }
+
+        else if (reactions[i].radius < 30)
+        {
+          reactions[i].radius = reactions[i].radius + 1;
+        }
+
+        if (reactions[i].radius == 0)
+        {
+          reactions.splice(i, 1);
+        }
+      }
+      
+      
+      for (var i = 0; i < balls.length; i++)
+      {
+        for (var j = 0; j < reactions.length; j++) 
+        {
+          var xdiff = (reactions[j].x - balls[i].x);
+          var ydiff = (reactions[j].y - balls[i].y);
+          var dist = Math.sqrt(xdiff * xdiff + ydiff * ydiff);
+          
+          //if collision, make new reaction object and add to array
+          if (dist <= (reactions[j].radius + balls[i].radius))
+          {
+            var new_reaction = {
+              radius: 10,
+              x: balls[i].x, //keeps track of x position
+              y: balls[i].y, //keeps track of y position
+              vx: 0, //keeps track of x velocity
+              vy: 0, //keeps track of y velocity
+              color: balls[i].color,
+              timer: 0
+            };
+
+            reactions.push(new_reaction);
+
+            numReacted = numReacted + 1;
+           
+            balls.splice(i, 1);
+
+            if (i >= 1)
+            {
+              i--;
+            }
+            
+          }
+
+        }
+      }
+
+      context.fillStyle = "black";
+
+      context.font = "20px Arial";
+
+      context.fillText("Reactions: " + numReacted, width/2 - 50, height/2);
+
+      if (reacting && reactions.length == 0)
+      {
+        menuText = "Game over!";
+        gameState = "menu";
+
       }
     }
-
-    context.fillStyle = "black";
-
-    context.font = "20px Arial";
-
-    context.fillText("Reactions: " + numReacted, width/2 - 50, height/2 );
-
+      
     requestAnimationFrame(updateGame);
   };
 
   // Handle a canvas click event
   $('#game_canvas').click(function(e) {
 
-    if(!reacting)
+    if (gameState == "menu")
+    {
+      gameState = "playing";
+
+      reacting = false;
+      numReacted = 0;
+
+      for (var i = 0; i < numBalls; i++)
+      {
+        modifier = -modifier;      
+
+        var b = {
+          radius: 10,
+          x: (width - 20) * Math.random() + 10, //keeps track of x position
+          y: (height - 20) * Math.random() + 10, //keeps track of y position
+          vx: modifier * 10 * Math.random(), //keeps track of x velocity
+          vy: modifier * 10 * Math.random(), //keeps track of y velocity
+          color: colors[Math.floor((Math.random()*colors.length))]
+        };
+
+        balls.push(b);
+      }
+
+    }
+
+    if(gameState == "playing" && !reacting)
     {
       // Find the mouse x and y relative to the top-left corner of the canvas
       var x = e.pageX - $(this).offset().left;
